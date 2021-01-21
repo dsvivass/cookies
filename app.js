b2d5547bb9582909c82153b6646fe5e21cbfe94f
dsvivass <dsvivass@unal.edu.co>
2021-01-21 18:22:48 -0500

    Prueba con creacion de cookies despues de cargada la pagina

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+console.log('adentro')
+
 document.cookie = 'name=Daniel'
+
+window.onload = () => {
+    var btn = document.getElementById('btn')
+
+btn.addEventListener('click', e => {
+    document.cookie = 'exploracion=UNA154'
+    console.log('click')
+})
+}
