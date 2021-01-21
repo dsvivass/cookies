187fa0f2872134b3929a3832e93d7587a67149d1
dsvivass <dsvivass@unal.edu.co>
2021-01-21 18:29:11 -0500

    Se agrega fecha de expiracion

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 console.log('adentro')
 
-document.cookie = 'name=Daniel'
+document.cookie = 'name=Daniel v; expires=Thu, 18 Dec 2025 12:00:00 UTC'
 
 window.onload = () => {
     var btn = document.getElementById('btn')
 
 btn.addEventListener('click', e => {
-    document.cookie = 'exploracion=UNA154'
+    document.cookie = 'exploracion=UNA154; expires=Thu, 18 Dec 2025 12:00:00 UTC'
     console.log('click')
 })
 }
